2224afd243d9b1698a9361d3cdf4b64b60a1d4bf
WEB Devise <manager@devise.com.ua>
2025-03-31 22:08:11 +0300

    Annotations and Author photos

--- js/dataFetcher.js
+++ js/dataFetcher.js
@@ -37,6 +37,7 @@ export const fetchLiveLibData = async (username, bookAnnotations, customPages) =
             'Date Read': book.readDate ? parseDate(book.readDate) : null,
             'Number of Pages': customPages[book.title] || book.details?.pages || 0
         }));
+        console.log(`Fetched ${booksData.length}`);
         updatedBooks.push(...booksData);
     }
     return updatedBooks;
